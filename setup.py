0413c1b04f35f6e208af5a0feed2432544738a97
YNKAnnapuna <136591876+Anu-0713@users.noreply.github.com>
2025-02-01 10:07:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name='audio-speech-to-sign-language-converter',
+    name='Voice-to-Sign-Language-translation-using-NLP',
     version='0.1.0',
     description='Python project',
-    author='Jigar Gajjar',
-    author_email='jigargajjar.55jg@gmail.com',
+    author='YNK Annapurna',
     url='https://github.com/jigargajjar55/Audio-Speech-To-Sign-Language-Converter',
     packages=setuptools.find_packages(),
     setup_requires=['nltk', 'joblib','click','regex','sqlparse','setuptools'],
